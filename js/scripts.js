94f60b70e204062c901577ef2abb9198bb0fbd89
Katy Roberts <letha.lillie@gmail.com>
2018-10-07 21:18:16 -0700

    working bare-bones ping-pong project

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var pingPong = function(number) {
+  $('ul li').remove();
+
+  for (var index = 1; index <= number; index += 1) {
+    if (index % 15 === 0) {
+      $('ul').append('<li>'+'Ping-pong!'+'</li>');
+
+    } else if (index % 3 === 0) {
+      $('ul').append('<li>'+'Ping'+'</li>');
+
+    } else if (index % 5 === 0) {
+      $('ul').append('<li>'+'Pong'+'</li>');
+
+    } else {
+
+    $('ul').append('<li>'+index+'</li>');
+  }
+  }
+};
+
+$(document).ready(function() {
+    $("form#ping-pong").submit(function(event) {
+      event.preventDefault();
+
+      var number = parseInt($("input#number").val());
+      var result = pingPong(number);
+  });
+  $("#result").show();
+});
